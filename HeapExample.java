481305fb86e106e6c7f6604f7ba58de0ff350b55
Deepa-3313 <venkatadeepaputhuru@gmail.com>
2025-06-09 13:52:58 +0530

    Add files via upload

---
+++ HeapExample.java
@@ -0,0 +1,34 @@
+package hello;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class HeapExample {
+
+	public static void main(String[] args) {
+		PriorityQueue<Integer>  minHeap = new PriorityQueue<>();
+		minHeap.add(60);
+		minHeap.add(40);
+		minHeap.add(20);
+		minHeap.add(0);
+		minHeap.add(5);
+		minHeap.add(30);
+		System.out.println("Min Heap Output");
+		while(!minHeap.isEmpty()) {
+			System.out.println(minHeap.poll());
+		}
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+		maxHeap.add(60);
+		maxHeap.add(40);
+		maxHeap.add(20);
+		maxHeap.add(0);
+		maxHeap.add(5);
+		maxHeap.add(30);
+		System.out.println("Max Heap Output");
+		while(!maxHeap.isEmpty()) {
+			System.out.println(maxHeap.poll());
+		}
+
+	}
+
+}
